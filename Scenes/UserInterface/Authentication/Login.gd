ca66d6bde0bed0fb98108ff2045f4fc93830f061
aditgocendra <gocendra123@gmail.com>
2022-01-30 12:35:38 +0800

    update firebase authentication

--- Scenes/UserInterface/Authentication/Login.gd
+++ Scenes/UserInterface/Authentication/Login.gd
@@ -1,16 +1,17 @@
 extends Control
 
 
+
 func _ready():
 	Firebase.Auth.connect("login_succeeded", self, "_on_LoginSuccess")
 	Firebase.Auth.connect("login_failed", self, "_on_LoginFailed")
-
-
+	
 func _on_Button_pressed():
-	var email = $VBoxContainer/Email.text
-	var password = $VBoxContainer/Password.text
+	var email = $Panel/VBoxContainer/Email.text
+	var password = $Panel/VBoxContainer/Password.text
 	
 	Firebase.Auth.login_with_email_and_password(email, password)
+	
 
 func _on_LoginSuccess(user : Dictionary):
 	print("Success")
@@ -18,3 +19,9 @@ func _on_LoginSuccess(user : Dictionary):
 func _on_LoginFailed(error_code, message):
 	print(error_code)
 	print(message)
+
+
+
+
+func _on_RedirectRegist_pressed():
+	get_parent().slide_menu("Register")
--- Scenes/UserInterface/Authentication/Register.gd
+++ Scenes/UserInterface/Authentication/Register.gd
@@ -1,27 +1,41 @@
 extends Control
 
+var collection : FirestoreCollection
 
 func _ready():
+	#Firebase Authentication
 	Firebase.Auth.connect("signup_succeeded", self, "_on_FirebaseSuccessSignUP")
 	Firebase.Auth.connect("signup_failed", self, "_on_FirebaseFailedSignUP")
-
+	
+	#Firebase Firestore
+	collection = Firebase.Firestore.collection('users')
+	collection.connect("get_document", self, "_on_get_document")
 
 
 func _on_Register_pressed():
-	var email = $VBoxContainer/Email.text
-	var password = $VBoxContainer/Password.text
-	var re_pass = $VBoxContainer/RePassword.text
-	
+	var email = $Panel/VBoxContainer/Email.text
+	var password = $Panel/VBoxContainer/Password.text
+	var re_pass = $Panel/VBoxContainer/RePassword.text
 	
 	if password == re_pass:
 		Firebase.Auth.signup_with_email_and_password(email, password)
 	else:
-		print("error")
+		print("Pass and re-pass not same")
 
 
 func _on_FirebaseSuccessSignUP(auth_info : Dictionary):
 	print("success")
 	
+	collection.get('3iBLsUhdhGiIiu7M3Z9r')
+	var document : FirestoreDocument = yield(collection, "get_document")
+	
 func _on_FirebaseFailedSignUP(error_code, message):
 	print(error_code)
 	print(message)
+
+func _on_get_document(document : FirestoreDocument):
+	print(document)
+
+
+func _on_RedirectLogin_pressed():
+	get_parent().slide_menu("Login")
